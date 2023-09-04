d8c00814e04e1357a638d3ee6d585678fa0b496a
Maria Eduarda <maria.eduarda.s19961@gmail.com>
2023-09-04 20:51:02 -0300

    nv
    add

---
+++ script.js
@@ -0,0 +1,25 @@
+// trocando os valores das variáveis 
+
+// ex 1
+let a = "preto";
+let b = "azul";
+
+let c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
+
+// ex 2 
+// nesse exemplo so vai aparecer a troca do valor num1
+
+var num1 = 5;
+var num2 = 9;
+
+var x = num1;
+
+num1=num2;
+num2=x;
+
+console.log(num1);
